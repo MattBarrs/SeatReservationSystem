8abfd49cbe7e63d0eecb8a95e71afe34dbd594b6
compSci-sc17mtab <73945959+compSci-sc17mtab@users.noreply.github.com>
2020-12-19 15:08:20 +0000

    jetsream & migration
    jetsream installed, livewire version used. Installed with the teams option
    
    migration files added and run for the db

---
+++ app/database/migrations/2020_11_29_142020_create_room_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRoomTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rooms', function (Blueprint $table) {
+            $table->id('roomID');
+            $table->string('room_name');
+            $table->time('close_time');
+            $table->time('open_time');
+            $table->string('booking_code');
+            $table->float('reference_length');
+            $table->string('floor_plan');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('room');
+    }
+}
---
+++ app/database/migrations/2020_11_29_142830_create_workstation_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateWorkstationTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('workstation', function (Blueprint $table) {
+            $table->id();
+            $table->string('roomID');
+            $table->string('seatID');
+            $table->json('availableExtra')->nullable();
+            $table->float('coord_x');
+            $table->float('coord_y');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('workstation');
+    }
+}
